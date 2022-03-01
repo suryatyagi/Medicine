b3dec2031209e7cd52d0b1c6b746a0ccf8ba6e9d
vvvvishnu10 <vishnu.01vishu@gmail.com>
2022-03-01 10:16:12 +0530

    code for sign up reset button of UC_AddUser.cs

--- MedicineMonitoring/MedicineMonitoring/function.cs
+++ MedicineMonitoring/MedicineMonitoring/function.cs
@@ -3,13 +3,13 @@ using System.Data.SqlClient;
 using System.Data;
 
 namespace Medisys
-{
+{ // after admin dashboard  1:46:10 timestamp
 	class function()
 	{// CONNECTION STRING
 	protected SqlConnection Getconnection()
 	{
 		SqlConnection con = new SqlConnection();
-		con.ConnectionString = "data source=LAPTOP-5VD4LSFK;database=Medisys;integrated security=True";
+		con.ConnectionString = "data source=LAPTOP-5VD4LSFK;database=Medisys;integrated security=True";// dataSource of everyones laptop will be different .
 
 		return con;
 	}
@@ -39,6 +39,63 @@ namespace Medisys
 
 	}
 
-		
+	// in public partial class of UC_AddUsers.cs
+	 function fn = new function();
+	  String query; 
+	*/
+
+
+
+
+
+	comment=after this we have to assign action to btn signup of UC_ADDUSER.cs  ( 2:01:42 )
+	  comment= adding a new user , we need these details
+	  comment=in btnSignUp_Click in UC_ADDUSER.cs file
+	  
+		code
+
+      String role = txtUserRole.Text;
+	  String f_Name = txtfName.Text; // first name
+	  String l_Name = txtlName.Text;     // last name 
+	  String dob = txtDob.Text;
+	  String gender = txtDob.Text;
+	  Int64 mobile = Int64.Parse(txtMobile.Text);
+	  String email = txtEmail.Text;
+	
+	  String username = txtUsername.Text;
+	  String pass = txtPassword.Text;
+	  
+	  try 
+	  {
+	    query = " insert into users (userRole,f_Name,l_Name,dob,gender,mobile,email,username,pass) values ('"+role+"','"+f_Name+"','"+l_Name+"','"+dob+"','"+gender+"','"+mobile+"','"+email+"','"+username+"','"+pass+"',)
+	    fn.setData(query,"Your Details are submitted Successfully")
+	  }
+	  catch
+	  {
+	    MessageBox.Show("Username already Exist.","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+	  }
+	   ///////
+     NOW FOR THE RESET BUTTON btnReset_Click
+	{
+	   clearAll();
+	  
+	
 	}
+	public void clearALL()
+	{
+	  txtfName.Clear();
+	  txtlName.Clear();
+	  txtdob.ResetText();
+	  txtgender.Clear();
+	  txtMobileno.Clear();
+	  txtEmail.Clear();
+	  txtUsername.Clear();
+	  txtPassword.Clear();
+	  txtUserRole.SelectedIndex =-1;
+	  
+	}
+
+
+	 
+}
 }
